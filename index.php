83e62c555da891786b0b088433a61cbf4b03205b
unknown <nguyen.bui1@live.vu.edu.au>
2014-09-15 00:36:26 +1000

    first

---
+++ index.php
@@ -0,0 +1,13 @@
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+</style>
+
+<div id="game"></div>
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="js/howler.min.js"></script>
+<script src="lib/pixi/bin/pixi.dev.js"></script>
+<script src="js/core.js"></script>
